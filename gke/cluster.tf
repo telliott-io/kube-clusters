fc020f9436b958af30629242e98a3eeffd4e65d3
Tom Elliott <tom.w.elliott@gmail.com>
2020-06-12 21:00:52 -0400

    Use the cluster's configuration directly

--- gke/cluster.tf
+++ gke/cluster.tf
@@ -31,19 +31,13 @@ resource "google_container_cluster" "primary" {
 # Retrieve an access token as the Terraform runner
 data "google_client_config" "provider" {}
 
-data "google_container_cluster" "my_cluster" {
-  depends_on = [google_container_cluster.primary]
-  name     = var.cluster_name
-  location = "us-central1"
-}
-
 output "kubernetes" {
   value = {
     load_config_file = false
-    host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+    host  = "https://${google_container_cluster.primary.endpoint}"
     token = data.google_client_config.provider.access_token
     cluster_ca_certificate = base64decode(
-      data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+      google_container_cluster.primary.master_auth[0].cluster_ca_certificate,
     )
     username = null
     password = null
